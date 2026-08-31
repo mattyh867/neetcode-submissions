d854c27ab82c66632995887791d5d010270b6203
Matthew Hughes <97196709+mattyh867@users.noreply.github.com>
2026-08-31 17:51:46 +0100

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        if len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return 1
+
+        ans = 0
+        counter = 0
+        hashTable = dict()
+
+        for num in nums:
+            hashTable[num] = True
+
+        x = min(hashTable.keys())
+        while len(hashTable) > 0:
+            # print(f'x={x} hashTable={hashTable}')
+            if x in hashTable.keys():
+                hashTable.pop(x)
+                counter += 1
+                ans = max(ans, counter)
+                x += 1
+            elif len(hashTable) > 0:
+                x = min(hashTable.keys())
+                counter = 0
+            else:
+                print("HUT")
+                ans = max(ans, counter)
+                return ans
+
+        ans = max(ans, counter)
+        return ans
